7dc339dc8f9945e8a7fd56f871d1fc57d39e00be
Eugeny Shlyagin <54446451+Udj13@users.noreply.github.com>
2025-04-24 12:43:55 +0300

    Escort TH BLE

--- model.dart
+++ model.dart
@@ -1,19 +1,21 @@
 import 'package:ble_inspector/widgets/escort_tw.dart';
+import 'package:ble_inspector/widgets/escort_th.dart';
+import 'package:ble_inspector/widgets/unknown.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_blue/flutter_blue.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 import 'widgets/escort_du.dart';
 import 'widgets/escort_td.dart';
 import 'widgets/escort_tl.dart';
 import 'widgets/mielta_fantom.dart';
 
-Widget? GetSensor(AdvertisementData data, String mac) {
+Widget? GetSensor(AdvertisementData data, String mac, bool showAllSensors) {
   // if (kDebugMode) {
   //   print(data);
   // }
 
-  if ((data.localName.isNotEmpty) && (data.localName.substring(0, 2) == 'TW')) {
+  if ((data.advName.isNotEmpty) && (data.advName.substring(0, 2) == 'TW')) {
     if (kDebugMode) print('Sensor TW found');
     if (kDebugMode) print(data);
 
@@ -38,7 +40,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
 
     if (ready) {
       return EscortTW(
-          name: data.localName,
+          name: data.advName,
           mac: mac,
           fuelData: fuelData,
           fuelLevel: fuelLevel,
@@ -48,7 +50,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
     }
   }
 
-  if ((data.localName.isNotEmpty) && (data.localName.substring(0, 2) == 'DU')) {
+  if ((data.advName.isNotEmpty) && (data.advName.substring(0, 2) == 'DU')) {
     // if (kDebugMode) print('Sensor DU found');
     // if (kDebugMode) print(data);
 
@@ -70,7 +72,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
 
     if (ready) {
       return EscortDU(
-          name: data.localName,
+          name: data.advName,
           mac: mac,
           angle: angle,
           mode: mode,
@@ -79,7 +81,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
     }
   }
 
-  if ((data.localName.isNotEmpty) && (data.localName.substring(0, 2) == 'MD')) {
+  if ((data.advName.isNotEmpty) && (data.advName.substring(0, 2) == 'MD')) {
     //print('Sensor MIELTA found');
 
     int percent = 0;
@@ -103,7 +105,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
 
     if (ready) {
       return MieltaFantom(
-          name: data.localName,
+          name: data.advName,
           mac: mac,
           fuelData: fuelLevel,
           fuelLevelInPercent: percent,
@@ -113,7 +115,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
     }
   }
 
-  if ((data.localName.isNotEmpty) && (data.localName.substring(0, 2) == 'TL')) {
+  if ((data.advName.isNotEmpty) && (data.advName.substring(0, 2) == 'TL')) {
     //print('Temp. sensor ESCORT found');
 
     int illumination = 0;
@@ -138,7 +140,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
 
     if (ready) {
       return EscortTL(
-          name: data.localName,
+          name: data.advName,
           mac: mac,
           temperature: temperature,
           illumination: illumination,
@@ -147,7 +149,7 @@ Widget? GetSensor(AdvertisementData data, String mac) {
     }
   }
 
-  if ((data.localName.isNotEmpty) && (data.localName.substring(0, 2) == 'TD')) {
+  if ((data.advName.isNotEmpty) && (data.advName.substring(0, 2) == 'TD')) {
     //print('Fuel level sensor ESCORT found');
 
     int fuelLevel = 0;
@@ -171,13 +173,70 @@ Widget? GetSensor(AdvertisementData data, String mac) {
 
     if (ready) {
       return EscortTD(
-          name: data.localName,
+          name: data.advName,
           mac: mac,
           fuelData: fuelData,
           fuelLevel: fuelLevel,
           temperature: temperature,
           battery: battery,
           firmware: firmware);
+    }
+  }
+
+  if ((data.advName.isNotEmpty) && (data.advName.substring(0, 2) == 'TH')) {
+    //print('Temp. sensor ESCORT found');
+
+    int battery = 0;
+    int temperature = 0;
+    int humidity = 0;
+    bool doorOpen = false;
+    int firmware = 0;
+    bool ready = false;
+
+    data.manufacturerData.forEach((key, value) {
+      if (value.length >= 11) {
+        temperature = (value[2] * 256 + value[1]) ~/ 10;
+        temperature > 127 ? temperature -= 256 : {};
+
+        humidity = (value[5] + value[6] * 256) ~/ 10;
+
+        battery = value[10];
+        doorOpen = value[9] == 11; // 11 - enabled, 3 - disabled
+        firmware = value[11];
+        ready = true;
+      }
+    });
+
+    if (ready) {
+      return EscortTH(
+          name: data.advName,
+          mac: mac,
+          temperature: temperature,
+          humidity: humidity,
+          battery: battery,
+          doorOpen: doorOpen,
+          firmware: firmware);
+    }
+  }
+
+  if (showAllSensors) {
+    bool ready = false;
+    String bleAdvertising = '';
+    data.manufacturerData.forEach((key, value) {
+      if (value.length != 0) {
+        value.forEach((element) {
+          bleAdvertising += '$value ';
+        });
+        ready = true;
+      }
+    });
+
+    if (ready) {
+      return UnknownSensor(
+        name: data.advName,
+        mac: mac,
+        bleAdvertising: bleAdvertising,
+      );
     }
   }
 
